73f9002bc7de47dc2e6ce9b372a94d2bf8f34f47
emascapa <emascapa@gmail.com>
2022-04-01 16:12:03 +0200

    Add palindromo section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,78 @@
+const elementForm1 = document.querySelector('form.form_1');
+
+const returnMessageDiv = document.querySelector('.form_1 .return_message');
+
+// al click controllo se la parola inserita è palindroma
+elementForm1.addEventListener('submit', function (event) {
+    // Stop page reloading
+    event.preventDefault()
+
+    const inputWord = document.querySelector('input.form-control').value;
+
+    console.log(`hai inserito come parola ${inputWord}`);
+
+    if (isPalindrome(inputWord)) {
+        const successMessage = `
+        <span class="text-success ms-3">La parola '${inputWord}' è palindroma!</span>
+        `;
+
+        returnMessageDiv.innerHTML = successMessage;
+
+        console.log(`'${inputWord}' è un palindromo`);
+
+    } else {
+
+        const successMessage = `
+        <span class="text-danger ms-3">La parola '${inputWord}' non è palindroma sry</span>
+        `;
+
+        returnMessageDiv.innerHTML = successMessage;
+
+        console.log(`'${inputWord}' non è un palindromo`);
+
+    }
+})
+
+
+
+const elementForm2 = document.querySelector('form.form_2');
+
+const gameResultDiv = document.querySelector('.game_result_div');
+
+ 
+elementForm2.addEventListener('submit', function (event) {
+    // Stop page reloading
+    event.preventDefault()
+
+    const inputTeam = document.querySelector('select.form-select').value;
+
+    const userNumber = document.getElementById('custom_range').value;
+
+    console.log(`hai scelto team ${inputTeam}`);
+    console.log(inputTeam);
+    console.log(`hai scelto il numero ${userNumber}`);
+
+    const pcNumber = getRndInteger(1, 5);
+
+    const sum = userNumber + pcNumber;
+
+    const result = isEven(sum);
+
+    if (result && inputTeam === 1) {
+
+    } else if () {
+        
+    } else if () {
+
+    } else if () {
+
+    }
+
+
+})
+
+
+
 
 
 function isPalindrome(word) {
@@ -18,13 +93,6 @@ function isPalindrome(word) {
     
     return output;
 }
-
-
-console.log(isPalindrome('anna'));
-console.log(isPalindrome('piccolo'));
-console.log(isPalindrome('ava'));
-console.log(isPalindrome('eva'));
-console.log(isPalindrome('ediolognomomongoloide'));
 
 
 
